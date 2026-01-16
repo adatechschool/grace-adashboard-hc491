b3193fc4425c83f454da7ffea922084fdc0b866f
Chau <hoaichau20@hotmail.com>
2026-01-16 23:18:36 +0100

    modal

--- grace-adashboard-hc491/src/App.jsx
+++ grace-adashboard-hc491/src/App.jsx
@@ -4,34 +4,65 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Skills } from "./components/Skills";
 import { Theme } from "./components/Theme";
+import { AddThemeForm } from "./components/AddthemeForm";
 
 function App() {
   const [bases, setBases] = useState([]);
-  const [showForm, setShowForm]= useState(false)
+  const [showForm, setShowForm] = useState(false);
+
+  //fonction pour récupérer tous les thèmes depuis API
+  const getBases = async () => {
+    const res = await fetch("http://localhost:3000/themes");
+    const data = await res.json();
+    setBases(data);
+  };
 
   useEffect(() => {
-    async function getBases() {
-      const res = await fetch("http://localhost:3000/themes");
-      const data = await res.json();
-      /* console.log(data); */
-      setBases(data);
-    }
-
     getBases();
   }, []);
 
-  const remove = async(id) => {
-   await fetch(`http://localhost:3000/themes/${id}`, {
+  //supprimer un thème
+  const remove = async (id) => {
+    await fetch(`http://localhost:3000/themes/${id}`, {
       method: "DELETE",
     });
-    setBases((value) => value.filter((theme) => theme.id !== id));
+    // setBases((value) => value.filter((theme) => theme.id !== id));
+    await getBases();
   };
+
+  //ajouter nouveau thème
+  const addTheme = async (newTheme) => {
+    const res = await fetch("http://localhost:3000/themes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTheme),
+    });
+
+    //recharge la liste depuis API pour etre sûr que react+base synchronisés
+    await getBases();
+
+    setShowForm(false);
+
+    // const createdTheme = await res.json();
+
+    // //mise àn jour du state
+    // setBases((prev) => [...prev, createdTheme]);
+  };
+
   return (
     <>
-    <div className="header">
-      <button className="add-theme-btn"
-      onClick={()=> setShowForm(!showForm)}>Ajouter un thème</button>
-    </div>
+      <div className="header">
+        <button className="add-theme-btn" onClick={() => setShowForm(true)}>
+          Ajouter un thème
+        </button>
+      </div>
+
+      {showForm && (
+        <AddThemeForm onAdd={addTheme} onClose={() => setShowForm(false)} />
+      )}
+
       <Theme bases={bases} onDelete={remove} />
     </>
   );
--- grace-adashboard-hc491/src/components/AddthemeForm.jsx
+++ grace-adashboard-hc491/src/components/AddthemeForm.jsx
@@ -1,7 +1,79 @@
-export const AddThemeForm = ({ onAdd, onClose}) => {
-    const
+import { useState } from "react";
 
+export const AddThemeForm = ({ onAdd, onClose }) => {
+  const [name, setName] = useState("");
+  const [skills, setSkills] = useState([{ label: "", validation: "KO" }]);
 
+  const addSkill = () => {
+    setSkills([...skills, { label: "", validation: "KO" }]);
+  };
 
+  const updateSkill = (index, field, value) => {
+    const newSkills = [...skills];
+    newSkills[index][field] = value;
+    setSkills(newSkills);
+  };
 
-}
+  const handleSubmit = async(e)=> {
+    e.preventDefault();
+
+    const newTheme = {
+      name,
+      skills,
+    };
+
+    await onAdd(newTheme);
+    onClose();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="add-theme-form">
+      <h2>Ajouter un thème</h2>
+
+      <input
+        type="text"
+        placeholder="Nom du thème"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+
+<div className="skills-section">
+      <h2>Compétences</h2>
+
+<div className="skills-list">
+      {skills.map((skill, index) => (
+        <div className="skill-row" key={index}>
+
+          <input
+            type="text"
+            placeholder="Compétence"
+            value={skill.label}
+            onChange={(e) => updateSkill(index, "label", e.target.value)}
+            required
+          />
+
+          <select
+            value={skill.validation}
+            onChange={(e) => updateSkill(index, "validation", e.target.value)}
+          >
+            <option value="KO">Pas acquis</option>
+            <option value="PROGRESS">En cours</option>
+            <option value="OK">Acquis</option>
+          </select>
+        </div>
+      ))}
+      </div>
+      </div>
+
+      <button type="button" onClick={addSkill}>
+        Ajouter une compétence
+      </button>
+
+      <button type="submit">Créer un thème</button>
+      <button type="button" onClick={onClose}>
+        🗑️
+      </button>
+    </form>
+  );
+};
